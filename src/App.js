faa8335e5585b01bfa306b51f4de085541fd3e8f
Emily Andrews <emilyandrews@Emilys-MacBook-Air.local>
2020-12-26 14:19:53 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Emily is Poopity <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Emily is awesome!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
+
           rel="noopener noreferrer"
         >
           Learn React
